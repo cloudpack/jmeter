759b21ab5d918128f8e6ac5e65dfb0c9bdef227b
Hiroyasu Suzuki <suzuki@suz-lab.com>
2015-06-14 04:44:01 +0000

    add absent script

---
+++ jmeter-absent.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+EXTRA_VARS=$(cat <<EOS
+    ProjectName=${ProjectName:-Jmeter0}
+    Region=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed -e 's/.$//'`
+    TemplateBucket=${TemplateBucket}
+    KeyName=${KeyName}
+    CustomerCidrIp=${CustomerCidrIp}
+    State=absent
+EOS
+)
+
+ansible-playbook -v -e "$EXTRA_VARS" ./ansible/cloudformation/jmeter.yml
